e24d66fa03879925ecdc037f9993d35c88ec3fe4
suraj-kumar12 <surajroman2588@gmail.com>
2026-06-20 07:42:03 +0530

    2nd time commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div>
       <h1 className='bg-[#000] text-white' >hello world!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, itaque nisi accusamus fugiat minima maxime eum debitis dolore iste architecto tempora magni? Nam labore, ex tempora vero veritatis repellendus cumque!</p>
     </div>
   )
 }
